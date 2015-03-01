40a46792eb8c0c609a0e1f22676ca37cf8fbb9b6
David Andrzejewski <david@davidandrzejewski.com>
2015-03-01 16:03:59 -0500

    Added ID functionality.

--- RepeaterController/RepeaterController.ino
+++ RepeaterController/RepeaterController.ino
@@ -1,3 +1,4 @@
+#include "morse.h"
 /*
 
 An Arduino-based repeater controller.
@@ -8,29 +9,37 @@ Author: David Andrzejewski
 */
 
 
+// Configure here.
 const int pinPtt = 2;
 const int pinCor = 3;
-
 const int pinPttLed = 13;
 
-int currentPttStatus = LOW;
-int currentCorStatus = LOW;
+// Default is to have PTT pulled to ground when engaged, and open when disengaged.
+const int pttEngaged = LOW;
+const int pttDisengaged = HIGH;
+
+// Timeout timer values
+const unsigned long timeoutTimer = 50000;
+
 
 // How often do we need to ID? A good value would probably be about 9.5 minutes.
-const unsigned long idTimerInterval = 30000;
+const unsigned long idTimerInterval = 10000;
+
+// ***** You probably do not need to touch anything below *****
 
 // Track the last keyup time
 unsigned long lastPttKeyupTime = 0;
 
 // Track the ID timer
 unsigned long idTimerStartTime = 0;
-boolean pendingId = false;
 
-// Timeout timer values
-const unsigned long timeoutTimer = 5000;
 unsigned long timeoutTimerStartTime = 0;
 
+int currentPttStatus = pttDisengaged;
+int pttStatusBeforeId = pttDisengaged;
+int currentCorStatus = LOW;
 boolean timedOut = false;
+boolean idInProgress = false;
 
 // Morse code ID values
 /*
@@ -45,18 +54,17 @@ Speed (wpm = 2.4 * dots per second
 
 */
 
-// All lengths in millis - 100ms = 24wpm
-const int dotLength = 100;
-const int pauseBetweenElements = dotLength;
-const int pauseBetweenCharacters = dotLength * 3;
-const int pauseBetweenWords = dotLength * 7;
+SpeakerMorseSender morse(4, 880, -1, 24);
 
 void setup() {
+  
+  Serial.begin(9600);
+  
   // Set up the PTT pin as an output
   pinMode(pinPtt, OUTPUT);
 
-  // Ensure we start with the PTT pin low.
-  digitalWrite(pinPtt, LOW);
+  // Ensure we start with the PTT pin high (off).
+  digitalWrite(pinPtt, pttDisengaged);
 
   // Set up the COR pin as an input
   pinMode(pinCor, INPUT);
@@ -64,96 +72,130 @@ void setup() {
   pinMode(pinPttLed, OUTPUT);
   digitalWrite(pinPttLed, LOW);
 
+  morse.setup();
+  morse.setMessage(String("kd8twg/r"));
+
 }
 
 void loop() {
   if (timedOut) {
     if (digitalRead(pinCor) == LOW) {
-        stopTimeoutTimer();
-      } else {
-        return;
-      }
+      stopTimeoutTimer();
+    } else {
+      return;
+    }
   }
 
-  // When COR goes high, PTT needs to go high.
-  if (digitalRead(pinCor) == HIGH && currentPttStatus == LOW) {
-    setPtt(HIGH);
+  // When COR goes high, PTT needs to engage.
+  if (digitalRead(pinCor) == HIGH && currentPttStatus == pttDisengaged) {
+    setPtt(pttEngaged, true);
   }
 
-  // When COR goes low, PTT needs to go low.
-  if (digitalRead(pinCor) == LOW && currentPttStatus == HIGH) {
-    setPtt(LOW);
+  // If we have an ID in progress, we need to keep PTT engaged.
+  if (currentPttStatus == pttDisengaged && idInProgress) {
+    setPtt(pttEngaged, false);
+  }
+
+  // When COR goes low, PTT needs to disengage (if no ID is in progress)
+  if (digitalRead(pinCor) == LOW && currentPttStatus == pttEngaged && !idInProgress) {
+    setPtt(pttDisengaged, false);
   }
 
 
   doIdIfNeeded();
 
-  doTimeoutIfNeeded();
-
+  if (!idInProgress) {
+    doTimeoutIfNeeded();
+  }
+  
 }
 
 // Sets the PTT pin and sets up timers related to PTT (ID and Timeout)
-void setPtt(int argPttHighLow) {
+void setPtt(int argPttHighLow, boolean startIdTimer) {
 
   // If we are just starting PTT, start the timeout timer.
-  if (argPttHighLow == HIGH && currentPttStatus == LOW) {
+  if (argPttHighLow == pttEngaged && currentPttStatus == pttDisengaged) {
     startTimeoutTimer();
   }
 
-  // If we are stopping PTT, shut off the timtout timer.
-  else if (argPttHighLow == LOW && currentPttStatus == HIGH) {
+  // If we are stopping PTT, shut off the timeout timer.
+  else if (argPttHighLow == pttDisengaged && currentPttStatus == pttEngaged) {
     stopTimeoutTimer();
-  }
-
-  // Start the ID timer - this does not get stopped or reset until ID happens.
-  if (argPttHighLow == HIGH) {
-    startIdTimerIfNeeded();
   }
 
   // Track current PTT status
   currentPttStatus = argPttHighLow;
 
-  // Set the PTT pin.
-  digitalWrite(pinPtt, argPttHighLow);
+  // Start the ID timer - this does not get stopped or reset until ID happens.
+  if (argPttHighLow == pttEngaged && startIdTimer) {
+    startIdTimerIfNeeded();
+  }
 
-  digitalWrite(pinPttLed, argPttHighLow);
+
+  // Set the PTT pin - but don't touch it if an ID is in progress.
+  
+  if (!idInProgress) {
+    digitalWrite(pinPtt, argPttHighLow);
+    digitalWrite(pinPttLed, !argPttHighLow);
+  }
 
 }
 
 // If the ID timer hasn't been started, start it.
 void startIdTimerIfNeeded() {
-  if (idTimerStartTime == 0 && digitalRead(pinPtt) == HIGH) {
+  if (idTimerStartTime == 0 && currentPttStatus == pttEngaged) {
+    Serial.println("Starting ID timer.");
     idTimerStartTime = millis();
   }
 }
 
 // If it's time to do an ID, do it.
 void doIdIfNeeded() {
-  if (millis() - idTimerStartTime > idTimerInterval) {
-    // Do the ID
+  if (!morse.continueSending()) {
+    idInProgress = false;
 
-    // Reset the timer
-    idTimerStartTime = 0;
+    if (millis() - idTimerStartTime > idTimerInterval && idTimerStartTime != 0) {
 
+      Serial.println("Starting morse ID...");
+
+      idTimerStartTime = 0;
+      
+      if (currentPttStatus == pttEngaged) {
+        startIdTimerIfNeeded();
+      }
+
+      setPtt(pttEngaged, false);
+    
+      delay(250);
+
+      idInProgress = true;
+
+      morse.startSending();
+
+    }
   }
+
 }
 
 // Start the timeout timer.
 void startTimeoutTimer() {
+  Serial.println("Starting timeout timer.");
   timeoutTimerStartTime = millis();
   timedOut = false;
 }
 
 // Stop the timeout timer.
 void stopTimeoutTimer() {
+  Serial.println("Stopping timeout timer.");
   timeoutTimerStartTime = 0;
   timedOut = false;
 }
 
 // Time out if needed.
 void doTimeoutIfNeeded() {
-  if (currentPttStatus == HIGH && millis() - timeoutTimerStartTime > timeoutTimer) {
-    setPtt(LOW);
+  if (currentPttStatus == pttEngaged && millis() - timeoutTimerStartTime > timeoutTimer) {
+    Serial.println("Timed out.");
+    setPtt(pttDisengaged, false);
     timedOut = true;
   }
 }
